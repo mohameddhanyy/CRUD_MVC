0862d4f8ae628acffd6a337f750923d8e951dc70
Mohameddhanyy <Mohamed_Hani_1179@fci.helwan.edu.eg>
2024-12-21 02:42:35 +0200

    Configure application with test first action

---
+++ CRUD/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public void Item(int id)
+        {
+        }
+    }
+}
