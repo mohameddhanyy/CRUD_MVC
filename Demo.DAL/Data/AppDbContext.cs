2b52aa06759f6025604b73fcca5b496965997447
Mohameddhanyy <Mohamed_Hani_1179@fci.helwan.edu.eg>
2024-12-22 19:06:05 +0200

    Add New Domain Model <Department'>

---
+++ Demo.DAL/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Demo.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Server = . ; Database = MVCApplication ; Trusted_Connection = True");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            => modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        DbSet<Department> departments { get; set; }
+
+    }
+}
---
+++ Demo.DAL/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Models
+{
+    internal class Department
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Code is Requires !!")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "Code is Requires !!")]
+        public string Name { get; set; }
+        public DateTime DateOfCreation { get; set; }
+    }
+}
