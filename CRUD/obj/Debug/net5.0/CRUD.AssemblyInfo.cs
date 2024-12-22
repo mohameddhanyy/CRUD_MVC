a0e2e382ea356c7792498d40c2330df265545dc2
Mohameddhanyy <Mohamed_Hani_1179@fci.helwan.edu.eg>
2024-12-22 18:36:29 +0200

    ReConfigure Project Structure based on 3-Tiers Architecture Pattern

--- CRUD/obj/Debug/net5.0/CRUD.AssemblyInfo.cs
+++ CRUD/obj/Debug/net5.0/CRUD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52b8d878687c7408aab0dae6225ced2a47ac30ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9c951d068deb0e2c4ec736fcf5392e46f3a5e38")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
