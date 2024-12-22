94ea69721590df6ffe6de59d53108ac8e0a8ed15
Mohameddhanyy <Mohamed_Hani_1179@fci.helwan.edu.eg>
2024-12-22 20:56:06 +0200

    Remove First Demo App(CRUD)

--- CRUD/obj/Debug/net5.0/CRUD.AssemblyInfo.cs
+++ CRUD/obj/Debug/net5.0/CRUD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73ae59e2777d0bb75087393aed882e428ecd7368")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b62b221804491e55af3f0ceae3bca231e2eef880")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
